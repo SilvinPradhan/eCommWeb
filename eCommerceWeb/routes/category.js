79c6211fcee1c07b6ad0355700a7a35cfd38166f
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-06-20 11:44:59 -0500

    product schema, create category implemented in the backend

--- eCommerceWeb/routes/category.js
+++ eCommerceWeb/routes/category.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router()
+const {requireSignIn, isAuth, isAdmin} = require("../controllers/auth")
 
 const {create} = require('../controllers/category')
+const {userById} = require('../controllers/user')
 
-router.post('/category/create', create);
+router.post('/category/create/:userId', requireSignIn, isAdmin, isAuth, create);
+router.param("userId", userById)
 
 module.exports = router;

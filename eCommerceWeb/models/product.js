a03aa64daba09bb0041fa554e2f058050c650c0b
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-06-20 12:27:34 -0500

    implemented product schema for a product in models

--- eCommerceWeb/models/product.js
+++ eCommerceWeb/models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const {ObjectId} = mongoose.Schema
 
 const productSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,33 @@ const productSchema = new mongoose.Schema({
         required: true,
         maxlength: 32
     },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 2000
+    },
+    price: {
+        type: Number,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    category: {
+        type: ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    quantity: {
+        type: Number
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    shipping: {
+        required: false,
+        type: Boolean
+    }
 }, {timestamps: true})
 
 module.exports = mongoose.model("Product", productSchema)

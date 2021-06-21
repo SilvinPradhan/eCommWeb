18781ab42a8d74f4db03b0ecfa5549160c63ce2e
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-06-21 18:36:45 -0500

    product item update method controller implemented with validation, admin resource restriction

--- eCommerceWeb/controllers/product.js
+++ eCommerceWeb/controllers/product.js
@@ -76,3 +76,46 @@ exports.remove = (req, res) => {
         })
     })
 }
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "The image could not be uploaded. Something wrong with the server."
+            })
+        }
+
+        //Check for all fields
+        const {name, description, price, category, quantity, shipping} = fields;
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All of the fields are to be filled."
+            })
+        }
+
+        let product = req.product
+        product = _.extend(product, fields)
+
+        if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "The image should be less than 1MB."
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result)
+        })
+    })
+}
+

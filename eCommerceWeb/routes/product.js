148825aed43920efbe9e5ae3c832871ff8e8c8f1
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-06-21 13:14:52 -0500

    implemented middleware for getting product by id and read single product

--- eCommerceWeb/routes/product.js
+++ eCommerceWeb/routes/product.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router()
 const {requireSignIn, isAuth, isAdmin} = require("../controllers/auth")
 
-const {create} = require('../controllers/product')
+const {create, productById, read} = require('../controllers/product')
 const {userById} = require('../controllers/user')
 
 router.post('/product/create/:userId', requireSignIn, isAdmin, isAuth, create);
+router.get('/product/:productId', read)
 router.param("userId", userById)
+router.param("productId", productById)
 
 module.exports = router;

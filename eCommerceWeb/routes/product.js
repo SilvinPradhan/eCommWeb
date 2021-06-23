7e5784714609118d3a99f0b6d13a760982609c9f
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-06-22 21:35:43 -0500

    business logic to list products by search in backend

--- eCommerceWeb/routes/product.js
+++ eCommerceWeb/routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router()
 const {requireSignIn, isAuth, isAdmin} = require("../controllers/auth")
 
-const {create, productById, read, remove, update, list, listRelated} = require('../controllers/product')
+const {create, productById, read, remove, update, list, listRelated, listCategories, listBySearch} = require('../controllers/product')
 const {userById} = require('../controllers/user')
 
 router.post('/product/create/:userId', requireSignIn, isAdmin, isAuth, create);
@@ -13,6 +13,8 @@ router.put('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, update
 
 router.get('/products', list)
 router.get('/products/related/:productId', listRelated)
+router.get('/products/categories', listCategories)
+router.post('/products/search', listBySearch)
 
 router.param("userId", userById)
 router.param("productId", productById)

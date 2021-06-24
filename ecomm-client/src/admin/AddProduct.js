f61d478d290c74140fbafb59d4e224e7b0b598da
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-06-24 11:45:17 -0500

    form to upload files or photos as product

--- ecomm-client/src/admin/AddProduct.js
+++ ecomm-client/src/admin/AddProduct.js
@@ -1,39 +1,48 @@
 import React, {useState, useEffect} from 'react'
 import {isAuthenticated} from "../auth/user"
 import {Link} from 'react-router-dom'
-import {Button, Container, TextField} from "@material-ui/core";
+import {Avatar, Button, Container, TextField} from "@material-ui/core";
 import {makeStyles} from '@material-ui/core/styles';
 import Layout from "../core/Layout";
-import BG from '../static/images/categoryBG/bg.jpg'
 import {createProduct} from './apiAdmin'
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faArrowLeft} from "@fortawesome/free-solid-svg-icons/faArrowLeft";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        minWidth: 275,
+    root: {},
+    heading: {
+        color: '#264653',
+        align: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: '#264653',
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
     },
     form: {
-        width: '50%',
+        width: '100%',
         marginTop: theme.spacing(1),
     },
     submit: {
         margin: theme.spacing(3, 0, 2),
         backgroundColor: '#264653',
     },
-    title: {
-        fontSize: 14,
-    },
-    container: {},
-    button: {
-        margin: theme.spacing(1),
-    },
     back: {
         cursor: 'pointer',
         textDecoration: 'none',
         color: '#ffffff'
+    },
+    container: {
+        minHeight: '300px'
     }
 }));
 
@@ -74,11 +83,78 @@ const AddProduct = () => {
         formData
     } = value
 
+    const change = (e) => {
+
+    }
+
+    const onSubmit = (e) => {
+
+    }
+
     const NewProductForm = () => (
         <>
-            <form className={"mb-3"}>
-                <h1>Product Form</h1>
-            </form>
+            <Container component="main" maxWidth="xs">
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography className={classes.heading}>
+                        <i className="fas fa-user"></i> Create Your Account
+                    </Typography>
+
+                    <form
+                        className={classes.form}
+                        onSubmit={(e) => onSubmit(e)}
+                    >
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            label="name"
+                            name="name"
+                            fullWidth
+                            autoFocus
+                            value={name}
+                            onChange={(e) => {
+                                change(e);
+                            }}
+                        ></TextField>
+                        <TextField
+                            variant="outlined"
+                            fullWidth
+                            autoFocus
+                            name="description"
+                            margin="normal"
+                            label="Description"
+                            value={description}
+                            onChange={(e) => {
+                                change(e);
+                            }}
+                        ></TextField>
+
+                        <TextField
+                            variant="outlined"
+                            fullWidth
+                            autoFocus
+                            type="number"
+                            name="price"
+                            margin="normal"
+                            label="Price"
+                            value={price}
+                            onChange={(e) => {
+                                change(e);
+                            }}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            className={classes.submit}
+                        >
+                            Register
+                        </Button>
+                    </form>
+                </div>
+            </Container>
         </>
     )
 

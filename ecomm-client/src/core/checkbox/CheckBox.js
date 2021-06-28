5f5c46253bbfb751bcf99983360cb02ff19a3d17
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-06-27 19:00:22 -0500

    implement handle toggle on checkbox for categories with category Ids

--- ecomm-client/src/core/checkbox/CheckBox.js
+++ ecomm-client/src/core/checkbox/CheckBox.js
@@ -1,11 +1,27 @@
 import React, {useEffect, useState} from 'react'
 
 const CheckBox = ({categories}) => {
+    const [checked, setChecked] = useState([])
+    const handleToggle = category => () => {
+        const initialCategoryId = checked.indexOf(category) // Return 1st index or -1
+        const newCheckedCategoryId = [...checked]
+        //    If currently checked was not already in the checked state, then push
+        if (initialCategoryId === -1) {
+            newCheckedCategoryId.push(category)
+        } else {
+            //    Else pop/splice
+            newCheckedCategoryId.splice(initialCategoryId, 1)
+        }
+        console.log(newCheckedCategoryId)
+        setChecked(newCheckedCategoryId)
+    }
     return (
         <>
             {
                 categories.map((category, index) => {
                     return <li key={index} className="list-unstyled"><input type="checkbox"
+                                                                            onChange={handleToggle(category._id)}
+                                                                            value={checked.indexOf(category._id === -1)}
                                                                             className="form-check-input"/>
                         <label className="form-check-label">{category.name}</label>
                     </li>

40819d51b3d3e3d70fbb17f51d6801ce4ea36a44
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-07-07 08:43:43 -0500

    payment transaction gateway implementation in the backend

--- eCommerceWeb/routes/braintree.js
+++ eCommerceWeb/routes/braintree.js
@@ -3,9 +3,10 @@ const router = express.Router()
 
 const {requireSignIn, isAuth} = require("../controllers/auth")
 const {userById} = require("../controllers/user")
-const {genToken} = require("../controllers/braintree")
+const {genToken, processPayment} = require("../controllers/braintree")
 
 router.get('/braintree/getToken/:userId', requireSignIn, isAuth, genToken);
+router.post('/braintree/payment/:userId', requireSignIn, isAuth, processPayment);
 
 router.param('userId', userById)
 

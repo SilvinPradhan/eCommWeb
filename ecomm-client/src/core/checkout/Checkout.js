6b185483b6c3dc0d8be7761d22f0e0581010b465
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-07-07 22:13:11 -0500

    reshaped the UI for the checkout with paypal integration dropin

--- ecomm-client/src/core/checkout/Checkout.js
+++ ecomm-client/src/core/checkout/Checkout.js
@@ -1,25 +1,27 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState, useEffect} from 'react';
 import {getBrainTreeClientToken, processPayment} from "../apiCore";
-import Card from '../cards/Card'
-import {emptyCart} from "../cart/cartHandler";
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
+import DropIn from 'braintree-web-drop-in-react';
+import {CircularProgress} from "@material-ui/core";
 import {isAuthenticated} from "../../auth/user";
-import DropIn from "braintree-web-drop-in-react";
-import {toast, ToastContainer} from "react-toastify";
+import {emptyCart} from "../cart/cartHandler";
 
-const Checkout = ({products}) => {
-
+const Checkout = ({products, setRun = f => f, run = undefined}) => {
     const [data, setData] = useState({
         loading: false,
         success: false,
         clientToken: null,
         error: '',
-        instance: {},
-        address: ''
-    })
+        instance: {
+            requestPaymentMethod() {
 
-    const userId = isAuthenticated() && isAuthenticated().user._id
-    const token = isAuthenticated() && isAuthenticated().token
+            }
+        },
+        address: ''
+    });
+
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
 
     const getToken = (userId, token) => {
         getBrainTreeClientToken(userId, token).then(data => {
@@ -34,112 +36,156 @@ const Checkout = ({products}) => {
     };
 
     useEffect(() => {
-        getToken(userId, token)
-    }, [])
+        getToken(userId, token);
+    }, [run]);
+
+    // const handleAddress = event => {
+    //     setData({ ...data, address: event.target.value });
+    // };
 
     const getTotal = () => {
         return products.reduce((currentValue, nextValue) => {
-            return currentValue + nextValue.count * nextValue.price
-        }, 0)
-    }
+            return currentValue + nextValue.count * nextValue.price;
+        }, 0);
+    };
 
     const showCheckout = () => {
-        return (
-            isAuthenticated() ? (<div className={"mt-2"}>{showDropIn()}</div>) : (
-                <Link to={"/signin"}>
-                    <button className={"btn btn-primary"}>Sign in to Checkout</button>
-                </Link>)
-        )
-    }
+        return isAuthenticated() ? (
+            <div>{showDropIn()}</div>
+        ) : (
+            <Link to="/signin">
+                <button className="btn btn-primary">Sign in to checkout</button>
+            </Link>
+        );
+    };
 
-    const purchase = () => {
-        setData({loading: true})
+    // let deliveryAddress = data.address;
+
+    const purchase = async () => {
+        setData({loading: true});
         // send the nonce to your server
         // nonce = data.instance.requestPaymentMethod()
         let nonce;
-        let getNonce = data.instance.requestPaymentMethod()
+        let getNonce = await data.instance
+            .requestPaymentMethod()
             .then(data => {
-                // console.log(data)
-                nonce = data.nonce
+                // console.log(data);
+                nonce = data.nonce;
+                // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
+                // and also total to be charged
+                // console.log(
+                //     "send nonce and total to process: ",
+                //     nonce,
+                //     getTotal(products)
+                // );
                 const paymentData = {
                     paymentMethodNonce: nonce,
                     amount: getTotal(products)
-                }
+                };
+
                 processPayment(userId, token, paymentData)
                     .then(response => {
-                        console.log(response)
-                        setData({...data, success: response.success})
-                        //    empty cart
+                        console.log(response);
+                        // empty cart
+                        // create order
+                        setData({
+                            loading: false,
+                            success: true
+                        });
                         emptyCart(() => {
-                            console.log('payment successful and empty cart')
-                            setData({loading: false, success: true})
-                        })
-                        //    create order
+                            setRun(!run); // run useEffect in parent Cart
+                            console.log('payment success and empty cart');
+                        });
+
+                        // const createOrderData = {
+                        //     products: products,
+                        //     transaction_id: response.transaction.id,
+                        //     amount: response.transaction.amount,
+                        //     address: deliveryAddress
+                        // };
+
+                        // createOrder(userId, token, createOrderData)
+                        //     .then(response => {
+                        //         emptyCart(() => {
+                        //             setRun(!run); // run useEffect in parent Cart
+                        //             console.log('payment success and empty cart');
+                        //             setData({
+                        //                 loading: false,
+                        //                 success: true
+                        //             });
+                        //         });
+                        //     })
+                        //     .catch(error => {
+                        //         console.log(error);
+                        //         setData({ loading: false });
+                        //     });
                     })
                     .catch(error => {
-                        console.log(error)
-                        setData({loading: false})
-                    })
-                toast.success(`Click 'Pay' to process to checkout! `, {
-                    position: "top-right",
-                    autoClose: 3000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                });
+                        console.log(error);
+                        setData({loading: false});
+                    });
             })
             .catch(error => {
-                console.log('DropIn Error', error)
-                setData({...data, error: error.message})
-            })
-    }
+                // console.log("dropin error: ", error);
+                setData({...data, error: error.message});
+            });
+    };
 
-    const showDropIn = () => {
-        return (
-            <div onBlur={() => setData({...data, error: ""})}>
-                {
-                    data.clientToken !== null && products.length > 0 ?
-                        (<div>
-                            <DropIn options={{
-                                authorization: data.clientToken,
-                                paypal: {
-                                    flow: "vault"
-                                }
-                            }} onInstance={instance => data.instance = instance}/>
-                            <button onClick={purchase} className={"btn btn-success btn-block"}>Pay</button>
-                        </div>) : null
-                }
-            </div>
-        )
-    }
+    const showDropIn = () => (
+        <div onBlur={() => setData({...data, error: ''})}>
+            {data.clientToken !== null && products.length > 0 ? (
+                <div>
+                    {/*<div className="gorm-group mb-3">*/}
+                    {/*    <label className="text-muted">Delivery address:</label>*/}
+                    {/*    <textarea*/}
+                    {/*        onChange={handleAddress}*/}
+                    {/*        className="form-control"*/}
+                    {/*        value={data.address}*/}
+                    {/*        placeholder="Type your delivery address here..."*/}
+                    {/*    />*/}
+                    {/*</div>*/}
 
-    const showError = (error) => {
-        return (
-            <div className={"alert alert-warning"} style={{display: error ? '' : 'none'}}>
-                {error}
-            </div>
-        )
-    }
+                    <DropIn
+                        options={{
+                            authorization: data.clientToken,
+                            paypal: {
+                                flow: 'vault'
+                            }
+                        }}
+                        onInstance={instance => (data.instance = instance)}
+                    />
+                    <button onClick={purchase} className="btn btn-success btn-block">
+                        Pay
+                    </button>
+                </div>
+            ) : null}
+        </div>
+    );
 
-    const showLoading = (loading) => (
-        loading && <h2>Loading...</h2>
-    )
+    const showError = error => (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    );
+
+    const showSuccess = success => (
+        <div className="alert alert-info" style={{display: success ? '' : 'none'}}>
+            Thanks! Your payment was successful!
+        </div>
+    );
+
+    const showLoading = loading => loading &&
+        <h2 className="text-danger"><CircularProgress variant={"determinate"} value={25}/></h2>;
 
     return (
         <div>
-            <ToastContainer/>
-            <h4>Total: ${getTotal()}</h4>
-            {
-                showError(data.error)
-            }
-            {
-                showCheckout()
-            }
+            <h2>Total: ${getTotal()}</h2>
             {showLoading(data.loading)}
+            {showSuccess(data.success)}
+            {showError(data.error)}
+            {showCheckout()}
         </div>
-    )
-}
+    );
+};
 
-export default Checkout
+export default Checkout;

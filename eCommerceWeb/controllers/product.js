b94716485a64b3e25d50f3808d575e3402019fec
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-07-08 22:32:08 -0500

    update product sold and quantity count businesss logic implementation,ie when the user checks out an item, decrease the product quantity and increase the sold quantity

--- eCommerceWeb/controllers/product.js
+++ eCommerceWeb/controllers/product.js
@@ -236,3 +236,26 @@ exports.listSearch = (req, res) => {
         }).select('-photo')
     }
 }
+
+exports.decreaseQuantity = (req, res, next) => {
+    let bulkOptions = req.body.order.products.map(item => {
+        return {
+            updateOne: {
+                filter: {
+                    _id: item._id
+                },
+                update: {
+                    $inc: {quantity: -item.count, sold: +item.count}
+                }
+            }
+        }
+    })
+    Product.bulkWrite(bulkOptions, {}, (error, products) => {
+        if (error) {
+            return res.status(400).json({
+                error: 'Could not update product'
+            })
+        }
+        next()
+    })
+}
--- eCommerceWeb/routes/order.js
+++ eCommerceWeb/routes/order.js
@@ -4,8 +4,9 @@ const router = express.Router()
 const {requireSignIn, isAuth} = require("../controllers/auth")
 const {userById, addOrderToUserHistory} = require("../controllers/user")
 const {create} = require("../controllers/order")
+const {decreaseQuantity} = require("../controllers/product")
 
-router.post('/order/create/:userId', requireSignIn, isAuth, addOrderToUserHistory, create)
+router.post('/order/create/:userId', requireSignIn, isAuth, addOrderToUserHistory, decreaseQuantity, create)
 
 router.param('userId', userById)
 

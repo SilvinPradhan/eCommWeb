55e50b83148d8ac7fb031f3421de80812aa9ee36
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-08-03 00:03:45 -0500

    ui for contact us page in progress, isomorphic fetch package used

--- ecomm-client/src/core/contactus/ContactUs.js
+++ ecomm-client/src/core/contactus/ContactUs.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
+import {emailContactForm} from '../../actions/form'
+import {Container, Grid, Input, TextareaAutosize, TextField} from '@material-ui/core'
+import {TextFieldsSharp} from "@material-ui/icons";
 
 const ContactUs = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            <h2>get in touch </h2>
-        </div>
+        <React.Fragment>
+            <div className={'contact'}>
+                <h4>Contact Information</h4>
+                <hr/>
+                <form onSubmit={handleSubmit}>
+                    <Container>
+                        <Grid xs={"12"} md={"6"}>
+                            <TextField variant="outlined"
+                                       margin="normal"
+                                       label="Name"
+                                       name="name"
+                                       fullWidth
+                                       autoFocus/>
+                        </Grid>
+                    </Container>
+                    <Container>
+                        <Grid xs={"12"} md={"6"}>
+                            <TextField variant="outlined"
+                                       margin="normal"
+                                       label="Email"
+                                       name="email"
+                                       fullWidth
+                                       autoFocus/>
+                        </Grid>
+                    </Container>
+                    <Container>
+                        <Grid xs={"12"} md={"12"}>
+                            <TextareaAutosize style={{width: '60vw', height: '50px'}} variant="outlined"
+                                              margin="normal"
+                                              label="Message"
+                                              placeholder={"Leave a Message"}
+                                              name="message"
+                                              fullWidth
+                                              autoFocus/>
+                        </Grid>
+                    </Container>
+
+                </form>
+            </div>
+        </React.Fragment>
     )
 }
 

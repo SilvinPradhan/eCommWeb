8577a95479a71a994abfdae4be465cbd76bebee5
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-09-29 19:13:18 -0500

    updated entry server file

--- eCommerceWeb/index.js
+++ eCommerceWeb/index.js
@@ -34,8 +34,6 @@ app.use(cors())
 // Database Configuration
 mongoose.connect(`${process.env.DATABASE}`, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
 }).then(() => console.log('Database Connected.'))
 mongoose.connection.on('error', err => {
